683fa2116eea501d21e6547229549b0632c212cd
ashutosh <ashutoshmali67@gmail.com>
2024-02-21 23:38:10 +0530

    commitall

--- Hotal_Management_System/Controllers/BookingController.cs
+++ Hotal_Management_System/Controllers/BookingController.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Hotal_Management_System.Models;
+using Hotal_Management_System.ViewModel;
 
 namespace Hotal_Management_System.Controllers
 {
     public class BookingController : Controller
     {
-        // GET: Booking
+        private HotalMSEntities objhotelDBEntities;
+
+        public BookingController()
+        {
+            objhotelDBEntities = new HotalMSEntities();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            ClsBooking objBookingViewMode = new ClsBooking();
+            objBookingViewMode.LisofRooms = (from objRoom in objhotelDBEntities.Rooms
+                                             where objRoom.BookingStatusId == 2
+                                             select new SelectListItem()
+                                             {
+                                                 Text = objRoom.RoomNo,
+                                                 Value = objRoom.RoomID.ToString()
+                                             }
+                                           ).ToList();
+            return View(objBookingViewMode);
         }
     }
 }
---
+++ Hotal_Management_System/ViewModel/ClsBooking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotal_Management_System.ViewModel
+{
+    public class ClsBooking
+    {
+        [Display(Name ="Customer Name")]
+        public string CustomerName { get; set; }
+        [Display(Name = "Customer Address")]
+        public string CustomerAddress { get; set; }
+        [Display(Name = "Customer Phone")]
+        public string CustomerPhoneNumber { get; set; }
+        [Display(Name = "Booking From")]
+        public DateTime BookingFrom { get; set; }
+        [Display(Name = "Booking To")]
+        public DateTime BookingTo { get; set; }
+        [Display(Name = "Assign RoomID")]
+        public int AssignRoomId { get; set; }
+        [Display(Name = "Number of Member")]
+        public int NoofMembers { get; set; }
+        //public string TotalAmounts { get; set; }
+
+        public IEnumerable<SelectListItem> LisofRooms { get; set; }
+           
+        //   public string BookingID { get; set; }
+    }
+}
